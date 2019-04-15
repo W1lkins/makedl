0ecbdfd4836c986eebe7eb7e6147328d9afc2dc2
W1lkins <github@wilkins.tech>
2019-04-15 16:26:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# makedl
+
+Fetch `Makefile` files from Github.
+
+**Table of Contents**
+
+<!-- toc -->
+
+- [Installation](#installation)
+    + [Via Go](#via-go)
+- [Usage](#usage)
+
+<!-- tocstop -->
+
+## Installation
+
+#### Via Go
+
+```console
+$ go get -u -v github.com/w1lkins/makedl
+```
+
+## Usage
+
+```console
+$ makedl [language]
+```
+
+This will download `[language].Makefile` from https://github.com/W1lkins/makefiles
+to the current working directory.
+
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const makeFmt = "https://raw.githubusercontent.com/w1lkins/makefiles/master/%s.Makefile"
+const path = "./Makefile"
+
+func main() {
+	if len(os.Args) == 1 {
+		log.Fatalf("usage: %s [language]", os.Args[0])
+	}
+	language := os.Args[1]
+
+	if language == "" {
+		log.Fatalf("lang required in form -lang=foo, -lang bar")
+	}
+
+	switch strings.ToLower(language) {
+	case "php":
+		language = "PHP"
+	case "go":
+		language = "Go"
+	case "golang":
+		language = "Go"
+	case "rust":
+		language = "Rust"
+	}
+	log.Printf("looking for makefile for language: %s", language)
+
+	url := fmt.Sprintf(makeFmt, language)
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if res.StatusCode == http.StatusNotFound {
+		log.Fatalf("makefile not found for language: %s", language)
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		log.Printf("writing new makefile for language: %s", language)
+		ioutil.WriteFile(path, bytes, 0644)
+	} else {
+		log.Print("refusing to write file since Makefile found in cwd")
+	}
+}
